442b38ad0037a33470061008e442157c9beadc66
Stefan Widgren <stefan.widgren@sva.se>
2022-10-24 11:47:46 +0200

    Import missing functions from RSQLite and SimInf

--- R/model.R
+++ R/model.R
@@ -19,8 +19,8 @@ init <- function() {
                  gamma  = 0.01)
 
     ## Add coordinates for the nodes to ldata.
-    model@ldata <- rbind(model@ldata, x = nodes$x)
-    model@ldata <- rbind(model@ldata, y = nodes$y)
+    model@ldata <- rbind(model@ldata, x = SimInf::nodes$x)
+    model@ldata <- rbind(model@ldata, y = SimInf::nodes$y)
 
     save(model, FALSE, "model.sqlite")
 
@@ -32,6 +32,11 @@ init <- function() {
 ##' @param append if TRUE, append data to the database, else create
 ##'     (or overwrite) data in the database.
 ##' @param path the path to the sqlite database.
+##' @importFrom RSQLite dbConnect
+##' @importFrom RSQLite dbDisconnect
+##' @importFrom RSQLite dbWriteTable
+##' @importFrom RSQLite SQLite
+##' @importFrom SimInf events
 ##' @noRd
 save <- function(model, append, path) {
     ## Open a database connection
