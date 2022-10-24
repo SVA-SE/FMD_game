5633480f077c1981fe43203e3e085434eb529df0
Thomas Rosendal <thomas.rosendal@sva.se>
2022-10-24 08:51:00 +0200

    continue sketch

--- model.R
+++ model.R
@@ -1,30 +1,41 @@
 library(RSQLite)
-ilibrary(SimInf)
+library(SimInf)
 
-## Create model
-u0 <- u0_SIR()
-u0$I[1] <- 1
-tspan <- seq(from = 1, to = 4*365, by = 1)
-model <- SIR(u0     = u0,
-             tspan  = tspan,
-             events = events_SIR(),
-             beta   = 0.16,
-             gamma  = 0.01)
+game_model_init <- function() {
+    u0 <- u0_SIR()
+    u0$I[1] <- 1
+    tspan <- seq(from = 1, to = 4*365, by = 1)
+    SIR(u0     = u0,
+        tspan  = tspan,
+        events = events_SIR(),
+        beta   = 0.16,
+        gamma  = 0.01)
+}
 
-## Open a database connection
-con <- dbConnect(SQLite(), "model.sqlite")
-
-## Init U
-U <- as.data.frame(t(model@u0))
-U <- cbind(node = seq_len(nrow(U)), time = 0L, U)
-dbWriteTable(con, "U", U, overwrite = TRUE)
-
-gdata <- c(beta = 2, gamma = 3)
-dbWriteTable(con, "gdata", gdata, overwrite = TRUE)
+model <- game_model_init()
 
 
-dbDisconnect(con)
+game_model_save <- function(model) {
+    ## Open a database connection
+    con <- dbConnect(SQLite(), "model.sqlite")
+    on.exit(expr = dbDisconnect(con))
 
+    ## Save the U state
+    U <- as.data.frame(t(model@u0))
+    U <- cbind(node = seq_len(nrow(U)), time = 0L, U)
+    dbWriteTable(con, "U", U, overwrite = TRUE)
+
+    ## TODO: We need to consider that to check if each is empty before
+    ## writing and then overwrite if true and append if false.
+
+    gdata <- model@gdata
+    dbWriteTable(con, "gdata", gdata, overwrite = TRUE)
+
+    ## TODO: If the gdata slot in the mode is empty what do we do?
+}
+
+
+game_model_step <- function(con) {
 ## Step
 con <- dbConnect(SQLite(), "model.sqlite")
 dbWriteTable(con, "U", u0_SIR(), append = TRUE)
@@ -33,3 +44,4 @@ dbDisconnect(con)
 con <- dbConnect(SQLite(), "model.sqlite")
 dbReadTable(con, "U")
 dbDisconnect(con)
+}
