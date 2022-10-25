ad06a11fec7cf18be16438ff5eedda7cde9832cd
Thomas Rosendal <thomas.rosendal@sva.se>
2022-10-25 14:36:47 +0200

    add loda function

--- R/model.R
+++ R/model.R
@@ -2,8 +2,9 @@
 ##'
 ##' @template dbname-param
 ##' @importFrom SimInf events_SIR
-##' @importFrom SimInf u0_SIR
+##' @importFrom Siminf n_nodes
 ##' @importFrom SimInf SIR
+##' @importFrom SimInf u0_SIR
 ##' @export
 init <- function(dbname = "./model.sqlite") {
     ## Create the initial population.
@@ -20,6 +21,7 @@ init <- function(dbname = "./model.sqlite") {
                  gamma  = 0.01)
 
     ## Add coordinates for the nodes to ldata.
+    model@ldata <- rbind(model@ldata, node = seq_len(n_nodes(model)))
     model@ldata <- rbind(model@ldata, x = SimInf::nodes$x)
     model@ldata <- rbind(model@ldata, y = SimInf::nodes$y)
 
@@ -91,16 +93,36 @@ step <- function(dbname = "./model.sqlite") {
 ##' @template dbname-param
 ##' @importFrom RSQLite dbConnect
 ##' @importFrom RSQLite dbDisconnect
-##' @importFrom RSQLite dbReadTable
-##' @importFrom RSQLite dbListTables
 ##' @importFrom RSQLite SQLite
+##' @importFrom RSQLite dbGetQuery
+##' @importFrom SimInf SIR
 ##' @noRd
 load <- function(dbname) {
     ## Open the database connection
     con <- dbConnect(SQLite(), dbname = dbname)
     on.exit(expr = dbDisconnect(con), add = TRUE)
-    tables <- dbListTables(con)
-    lapply(tables, function(x) {
-        dbReadTable(con, x)
-    })
+
+    sql <- "SELECT time FROM U WHERE time=(SELECT max(time) FROM U) LIMIT 1"
+    time <- as.numeric(dbGetQuery(con, sql))
+
+    sql <- "SELECT * FROM U WHERE time=:time ORDER BY node;"
+    u0 <- dbGetQuery(con, sql, params = c(time = time))
+
+    sql <- "SELECT * FROM ldata ORDER BY node;"
+    ldata <- dbGetQuery(con, sql)
+
+    sql <- "SELECT * FROM events WHERE time=:time;"
+    events <- dbGetQuery(con, sql, params = c(time = time + 1))
+
+    ## Create an SIR model. Note that beta and gamma will be replaced
+    ## when ldata is injected.
+    model <- SIR(u0     = u0,
+                 tspan  = time + 1,
+                 events = events,
+                 beta   = 0,
+                 gamma  = 0)
+
+    model@ldata <- t(as.matrix(ldata))
+
+    model
 }
