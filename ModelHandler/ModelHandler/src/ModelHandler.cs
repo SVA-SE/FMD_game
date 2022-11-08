c89d17785a0b991bcdd26e8833732683ad563ec7
VMormoris <vmormoris2014@gmail.com>
2022-11-08 15:21:57 +0100

    Update interaction with model version 2.2

--- ModelHandler/ModelHandler/src/ModelHandler.cs
+++ ModelHandler/ModelHandler/src/ModelHandler.cs
@@ -4,8 +4,12 @@ using System.Data.SQLite;
 using System.Diagnostics;
 using System.Threading;
 
+//Intensity is in range [0.0349315068493151, 0.0527397260273973]
+
 namespace ModelHandler
 {
+    delegate void ActionRef<T>(ref T arg);
+
     /**
      * <summary>Struct <c>Exchange</c>, data structure describing an Exchange between farms.</summary>
      * <see cref="Farm"/>
@@ -33,7 +37,7 @@ namespace ModelHandler
     /**
      * <summary>Struct <c>Farm</c>, data structure containing information about a Farm.</summary>
      */
-    public struct Farm
+    public class Farm
     {
         /// <summary>Farm's ID. Use ID-1 to find the index of the Farm on the list.</summary>
         public uint ID;
@@ -43,19 +47,65 @@ namespace ModelHandler
         public uint I;
         /// <summary>Number of cattle that have <b>Recovered</b> from the infection.</summary>
         public uint R;
+        /// <summary>The possibility to send animals each day</summary>
+        public double Intensity;
+
         /**
          * <summary>List of exchanges with this farm (aka Farm's log).</summary>
          * <see cref="Exchange"/>
          */
         public List<Exchange> Logs;
 
-        public Farm(uint id, uint s, uint i, uint r)
+        /// <summary> List of friends that this farms has.
+        /// Each element is basically a pair that contains a Farm's ID and "weight" value.
+        /// If a value with farm's ID 0 (usually the last one) means that the "weight" represents the posibility to send anywhere else.
+        /// </summary>
+        public List<Tuple<uint, float>> Connections;
+
+        private static Random sRandomEngine = new Random();
+        private static double sMinIntensity = 0.0349315068493151;
+        private static double sMaxIntensity = 0.0527397260273973;
+
+        public Farm(uint id, bool initialize = true)
         {
             ID = id;
+            Logs = new List<Exchange>();
+
+            if (!initialize)
+                return;
+
+            double elapsed = sMaxIntensity - sMinIntensity;
+            Intensity = sRandomEngine.NextDouble() * elapsed + sMinIntensity;
+
+            Connections = new List<Tuple<uint, float>>();
+            int Max = 101;
+            for (int aux = 0; aux < 10; aux++)
+            {
+                int to;
+                while ((to = sRandomEngine.Next(1, 151)) == id)
+                    to = sRandomEngine.Next(1, 151);
+                int rnd = sRandomEngine.Next(1, Max);
+                Max -= rnd;
+
+                float weight = (float)rnd / 100;
+                Connections.Add(new Tuple<uint, float>((uint)to, weight));
+                if (Max <= 1)
+                    break;
+            }
+
+            if (Max > 1)
+            {
+                float weight = (float)Max / 100;
+                Connections.Add(new Tuple<uint, float>(0, weight));
+            }
+        }
+
+        public Farm(uint id, uint s, uint i, uint r)
+            : this(id, false)
+        {
             S = s;
             I = i;
             R = r;
-            Logs = new List<Exchange>();
         }
     }
 
@@ -69,6 +119,7 @@ namespace ModelHandler
 
         private static ReaderWriterLock sLock = new ReaderWriterLock();
         private static Thread sThread = null;//Temporary
+        private static Random sRandomEngine = new Random();
 
         public static void Join() { sThread.Join(); } //Also Temporary
 
@@ -80,7 +131,23 @@ namespace ModelHandler
         {
             sThread = new Thread(() =>
             {
+                sFarms = new Farm[150];
+                for (int i = 0; i < sFarms.Length; i++)
+                    sFarms[i] = new Farm((uint)i + 1);
+
                 RunRScript("-e \"game.FMD::init('model.sqlite')\"");
+                
+                //Change database so it works only with 150 farms
+                ExecuteQuery("DELETE FROM U WHERE node > 150;");
+                ExecuteQuery("UPDATE U SET I = 0;");
+                ExecuteQuery("DELETE FROM ldata WHERE node > 150;");
+                ExecuteQuery("DELETE FROM events;");
+
+                //Create zero patient at random
+                int zeroPatient = sRandomEngine.Next(1, 151);
+                string query = string.Format("UPDATE U SET I = 1 WHERE node = {0};", zeroPatient);
+                ExecuteQuery(query);
+
                 _RunModel(new List<uint>(), days);
             });
             sThread.Start();
@@ -107,9 +174,9 @@ namespace ModelHandler
                 if (quarantine.Count > 0)
                     UpdateEvents(quarantine);
 
+                CreateEvents();
                 RunRScript("-e \"game.FMD::run('model.sqlite')\"");
                 UpdateFarms();
-                UpdateLogs();
 
                 sCurrentDay++;
             }
@@ -122,46 +189,25 @@ namespace ModelHandler
                 query += " node = " + quarantine[i] + " OR dest = " + quarantine[i] + " OR";
             query += " node = " + quarantine[quarantine.Count - 1] + " OR dest = " + quarantine[quarantine.Count - 1] + " );";
 
-            SQLiteConnection conn = new SQLiteConnection("Data Source=model.sqlite");
-            conn.Open();
-            using (var cmd = conn.CreateCommand())
-            {
-                cmd.CommandText = query;
-                cmd.ExecuteNonQuery();
-            }
-            conn.Close();
+            ExecuteQuery(query);
         }
 
         private static void UpdateFarms()
         {
             List<Farm> farms = new List<Farm>();
-            SQLiteConnection conn = new SQLiteConnection("Data Source=model.sqlite");
-            conn.Open();
-            using (var cmd = conn.CreateCommand())
+            string query = string.Format("SELECT node, S, I, R FROM U WHERE time = {0};", sCurrentDay);
+            ExecuteQuery(query, true, (ref SQLiteDataReader reader) =>
             {
-                cmd.CommandText = "SELECT node, S, I, R FROM U WHERE time = " + sCurrentDay + ';';
-                var reader = cmd.ExecuteReader();
-                while(reader.Read())
-                {
-                    uint ID = (uint)reader.GetInt32(0);
-                    uint S = (uint)reader.GetInt32(1);
-                    uint I = (uint)reader.GetInt32(2);
-                    uint R = (uint)reader.GetInt32(3);
+                uint ID = (uint)reader.GetInt32(0);
+                uint S = (uint)reader.GetInt32(1);
+                uint I = (uint)reader.GetInt32(2);
+                uint R = (uint)reader.GetInt32(3);
 
-                    farms.Add(new Farm(ID, S, I, R));
-                }
-            }
-            conn.Close();
+                farms.Add(new Farm(ID, S, I, R));
+            });
 
             //Publish results
             sLock.AcquireWriterLock(-1);
-            if (sFarms == null)
-            {
-                sFarms = new Farm[farms.Count];
-                for(int i = 0; i<sFarms.Length; i++)
-                    sFarms[i].Logs = new List<Exchange>();
-            }
-            
             for(int i = 0; i<farms.Count; i++)
             {
                 Farm farm = farms[i];
@@ -173,42 +219,98 @@ namespace ModelHandler
             sLock.ReleaseWriterLock();
         }
 
-        private static void UpdateLogs()
+        private static void CreateEvents()
         {
             //Make local copy of farm array
             sLock.AcquireReaderLock(-1);
             List<Farm> farms = new List<Farm>(sFarms);
-            uint day = sCurrentDay + 1;
             sLock.ReleaseReaderLock();
 
-            SQLiteConnection conn = new SQLiteConnection("Data Source=model.sqlite");
-            conn.Open();
-            using(var cmd = conn.CreateCommand())
+            List<Exchange> events = new List<Exchange>();
+            foreach(Farm farm in farms)
             {
-                //TODO(Vasilis): If we change log to propotions change what we are actually looking for
-                cmd.CommandText = "SELECT node, dest, n FROM events WHERE event = \"extTrans\" AND time = " + day + ';';
-                var reader = cmd.ExecuteReader();
-                while(reader.Read())
+                double p = sRandomEngine.NextDouble();
+                if (p > farm.Intensity)//Check the farm should make a transfer today
+                    continue;
+
+                //Find out to which of it's friends he will send
+                p = sRandomEngine.NextDouble();
+                uint to = 0;
+                float lucklyhood = 0.0f;
+                bool found = false;
+                List<uint> friends = new List<uint>(farm.Connections.Count);
+                foreach(Tuple<uint, float> friend in farm.Connections)
                 {
-                    int from = reader.GetInt32(0);
-                    int to = reader.GetInt32(1);
-                    uint n = (uint)reader.GetInt32(2);
+                    lucklyhood += friend.Item2;
+                    if(lucklyhood <= p && !found)
+                    {
+                        to = friend.Item1;
+                        found = true;
+                    }
 
-                    Farm src = farms[from - 1];//Source farm
-                    Farm dst = farms[to - 1];//Destination farm
-
-                    src.Logs.Add(new Exchange((uint)from, (uint)to, n, day));
-                    dst.Logs.Add(new Exchange((uint)from, (uint)to, n, day));
+                    friends.Add(friend.Item1);
                 }
+
+                if(to == 0)//Selected farm means he/she could to anybody so we choose one randomly that is not the same farm on of it's friends
+                {
+                    to = (uint)sRandomEngine.Next(1, 151);
+                    while (to == farm.ID || friends.Contains(to))
+                        to = (uint)sRandomEngine.Next(1, 151);
+                }
+
+                Exchange exchange = new Exchange(farm.ID, to, 1, sCurrentDay);
+                events.Add(exchange);
+
+                Farm src = farms[(int)farm.ID - 1];
+                Farm dst = farms[(int)to - 1];
+                
+                src.Logs.Add(exchange);
+                dst.Logs.Add(exchange);
             }
-            conn.Close();
+
+            foreach(Exchange exchange in events)
+            {
+                string query = string.Format("INSERT INTO events (event, time, node, dest, n, proportion, 'select', shift) VALUES(\"extTrans\", {0}, {1}, {2}, {3}, 0.0, 4, 0);", sCurrentDay, exchange.From, exchange.To, 1);
+                ExecuteQuery(query);
+            }
 
             //Publish changes
             sLock.AcquireWriterLock(-1);
+            //TODO(Vasilis): Remove old logs 
             for (int i = 0; i < farms.Count; i++)
                 sFarms[i].Logs = farms[i].Logs;
             sLock.ReleaseWriterLock();
         }
+
+        //private static void UpdateLogs()
+        //{
+        //    //Make local copy of farm array
+        //    sLock.AcquireReaderLock(-1);
+        //    List<Farm> farms = new List<Farm>(sFarms);
+        //    uint day = sCurrentDay + 1;
+        //    sLock.ReleaseReaderLock();
+        //
+        //    //TODO(Vasilis): If we change log to propotions change what we are actually looking for
+        //    string query = string.Format("SELECT node, dest, n FROM events WHERE event = \"extTrans\" AND time = {0};", day);
+        //    ExecuteQuery(query, true, (ref SQLiteDataReader reader) =>
+        //    {
+        //        int from = reader.GetInt32(0);
+        //        int to = reader.GetInt32(1);
+        //        uint n = (uint)reader.GetInt32(2);
+        //
+        //        Farm src = farms[from - 1];
+        //        Farm dst = farms[to - 1];
+        //
+        //        src.Logs.Add(new Exchange((uint)from, (uint)to, n, day));
+        //        dst.Logs.Add(new Exchange((uint)from, (uint)to, n, day));
+        //    });
+        //
+        //    //Publish changes
+        //    sLock.AcquireWriterLock(-1);
+        //    for (int i = 0; i < farms.Count; i++)
+        //        sFarms[i].Logs = farms[i].Logs;
+        //    sLock.ReleaseWriterLock();
+        //}
 
         private static void RunRScript(string args)
         {
@@ -222,6 +324,24 @@ namespace ModelHandler
             Rscript.WaitForExit();
         }
 
+        private static void ExecuteQuery(string query, bool selection = false, ActionRef<SQLiteDataReader> action = null)
+        {
+            SQLiteConnection conn = new SQLiteConnection("Data Source=model.sqlite");
+            conn.Open();
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = query;
+                if (!selection)
+                    cmd.ExecuteNonQuery();
+                else
+                {
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                        action(ref reader);
+                }
+            }
+            conn.Close();
+        }
 
     }
 }
--- ModelHandler/ModelHandler/src/Program.cs
+++ ModelHandler/ModelHandler/src/Program.cs
@@ -10,29 +10,29 @@ namespace ModelHandler
         static void Main(string[] args)
         {
             Directory.SetCurrentDirectory("../../../..");
-            //ModelHandler.Init(90);
-            //ModelHandler.Join();
+            ModelHandler.Init(7);
+            ModelHandler.Join();
             Console.WriteLine("Done.");
             
-            //SQLiteConnection conn = new SQLiteConnection("Data Source=model.sqlite");
-            //conn.Open();
-            //using (var cmd = conn.CreateCommand())
-            //{
-            //    cmd.CommandText = "SELECT node, S, I, R FROM U WHERE time = 90 AND I > 0";
-            //    var reader = cmd.ExecuteReader();
-            //    while (reader.Read())
-            //    {
-            //        uint ID = (uint)reader.GetInt32(0);
-            //        uint S = (uint)reader.GetInt32(1);
-            //        uint I = (uint)reader.GetInt32(2);
-            //        uint R = (uint)reader.GetInt32(3);
-            //
-            //        Console.WriteLine("Farm[{0}]: S {1}, I {2}, R {3}", ID, S, I, R);
-            //    }
-            //}
-            //conn.Close();
-            //Console.WriteLine("Press any key to continue...");
-            //Console.Read();
+            SQLiteConnection conn = new SQLiteConnection("Data Source=model.sqlite");
+            conn.Open();
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = "SELECT node, S, I, R FROM U WHERE time = 2 AND I > 0";
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    uint ID = (uint)reader.GetInt32(0);
+                    uint S = (uint)reader.GetInt32(1);
+                    uint I = (uint)reader.GetInt32(2);
+                    uint R = (uint)reader.GetInt32(3);
+            
+                    Console.WriteLine("Farm[{0}]: S {1}, I {2}, R {3}", ID, S, I, R);
+                }
+            }
+            conn.Close();
+            Console.WriteLine("Press any key to continue...");
+            Console.Read();
         }
     }
 }
